9970b6af00b2376670eb5943b862d7f8200f0ff6
GIULIO ARECCO <giulioarecco@gmail.com>
2025-06-04 14:50:43 +0200

    Add initial server setup

--- server/index.mjs
+++ server/index.mjs
@@ -1,9 +1,24 @@
 // imports
 import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
 
 // init express
 const app = new express();
 const port = 3001;
+
+// middleware
+app.use(express.json());
+app.use(morgan('dev'));
+
+// cors setup
+const corsOptions = {
+    origin: 'http://localhost:5173', 
+    optionSuccessStatus: 200, 
+    credentials: true
+};
+
+app.use(cors(corsOptions));
 
 // activate the server
 app.listen(port, () => {
